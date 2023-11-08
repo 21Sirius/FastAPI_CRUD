73e40d95954bfe69896e37174d427aaf1d787f20
21Sirius <21siriusss@gmail.com>
2023-11-08 19:08:35 +0700

    Перенос items в отдельный файл

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI
 
+from fastapi import FastAPI,
+from pydantic import EmailStr, BaseModel
 import uvicorn
 
-
-
 app = FastAPI()
+
+
+class CreateUser(BaseModel):
+    email: EmailStr
 
 
 @app.get("/")
@@ -13,10 +16,19 @@ def hello_index():
         "message": "Hello index!",
     }
 
+
 @app.get("/hello/")
 def hello(name: str = "World"):
     name = name.strip().title()
     return {"message": f"Hello {name}!"}
+
+
+@app.post("/users/")
+def create_user(user: CreateUser):
+    return {
+        "message": "success",
+        "email": user.email,
+    }
 
 
 @app.get("/calc/add/")
@@ -28,5 +40,8 @@ def add(a: int, b: int):
     }
 
 
+
+
+
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)/
+    uvicorn.run("main:app", reload=True)
